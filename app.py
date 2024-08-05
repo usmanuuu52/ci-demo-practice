0b580fd195384dc352dd86c0b4ff1ffc7c7a4b2f
Usmanuuu52 <usmankhanuuu52@gmail.com>
2024-08-05 16:57:51 +0500

    minor typo

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 def add(a, b):
-    return a+b
+    return a + b
 
 
 def sub(a, b):
-    return a-b
+    return a - b
+
 
 result1=add(4, 5)
 result2=sub(50, 33)
 
-print(result1,result2)
+print(result1, result2)

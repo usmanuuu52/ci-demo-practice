0d541cdb617e5fede614b24bc7d11859327c8907
Usmanuuu52 <usmankhanuuu52@gmail.com>
2024-08-05 16:55:55 +0500

    minor typo

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-def add(a,b):
+def add(a, b):
     return a+b
 
 
+def sub(a, b):
+    return a-b
 
-
-
-def sub(a,b): return a-b
-
-
-
-
-result1=add(4,5)
-result2=sub(50,33)
-
+result1=add(4, 5)
+result2=sub(50, 33)
 
 print(result1,result2)
